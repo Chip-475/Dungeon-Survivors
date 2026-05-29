4fe7ef96668359be127bf95a365393eb2c3c6e11
mastery0 <giulio.dibartolo23@gmail.com>
2026-05-30 00:14:55 +0200

    sprites

--- Assets/Scripts/Cards/fireDamage/fireAspectCard.cs
+++ Assets/Scripts/Cards/fireDamage/fireAspectCard.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class fireAspectCard : cardClass, ICardEffect
 {
+    public Sprite FireScythe;
+    public GameObject scythe;
     private void effect()
     {
         data.fireAspectLvl++;
         print("fireAspect picked");
+            if (data.fireAspectLvl == 1)
+            {
+                scythe.GetComponent<SpriteRenderer>().sprite = FireScythe;
+        }
     }
     public void cardEffect()
     {
