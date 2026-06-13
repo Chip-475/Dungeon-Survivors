1ee51dc9e4c4ce8f6802f42768702a0f4ca5da44
Alfio <alfiocutuli08@gmail.com>
2026-06-13 13:18:14 +0200

    adding new sprites  and working on new feature and bug fix

--- Assets/Scripts/floorTiles.cs
+++ Assets/Scripts/floorTiles.cs
@@ -4,6 +4,7 @@ using UnityEngine.Tilemaps;
 using System.Collections.Generic;
 public class floorTiles : MonoBehaviour
 {
+    public static floorTiles instance;
     public Tilemap tilemap;
     public List<TileBase> tiles = new();
     public List<TileBase> roadTiles = new();
