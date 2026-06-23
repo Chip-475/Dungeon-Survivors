bfb18e24eec579c037009953728e081a64a72cc8
Alfio <alfiocutuli08@gmail.com>
2026-06-23 17:15:18 +0200

    tileset and map adjustment and other bug fix

--- Assets/Scripts/UI/enemyHP.cs
+++ Assets/Scripts/UI/enemyHP.cs
@@ -7,11 +7,11 @@ public class enemyHP : MonoBehaviour
     public Image hpBar;
     public Quaternion rot;
     private Transform parentrans;
-    /*
-     *
+    
+     
     void Start()
     {
-        parentrans = this.transform.parent.parent.parent;
+        parentrans = gameObject.transform.parent.parent.parent;
     }
     void Update()
     {
@@ -28,14 +28,6 @@ public class enemyHP : MonoBehaviour
             GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
         }
     }
-    */
-    void Update()
-    {
-        if (enemyClass == null) return;
-        hpBar.fillAmount = enemyClass.hp / enemyClass.hpMax;
-        float scalex = enemyClass.transform.localScale.x;//cosi la catena non riscia di essere null (giulio shit)
-        RectTransform rectTransform = GetComponent<RectTransform>();
-        if (scalex < 0) rectTransform.localScale = new Vector3(-1, 1, 1);
-        else rectTransform.localScale=new Vector3(1, 1, 1);
-    }
+ 
+    
 }
